4b7960a619681b1a37c97ae784bec888c4fa950e
Mark Kendall <mark.kendall@kcftech.com>
2020-05-16 16:46:58 -0500

    MakeClass

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import markimage from './MarkKendall.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={markimage} className="markimage" alt="markimage" />
-        <a
-          className="App-link"
-          href="https://www.reverbnation.com/TheNuKendalls"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Mark Kendall Band
-        </a>
-        
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React for the Best Job on the planet! Mr Mark Kendall of KCF Technologies.
-        </a>
-        <a
-          className="App-link"
-          href="https://kendallsoft-dd2b9.firebaseapp.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Angular Site
-        </a>
-        
-      </header>
-    </div>
-  );
-}
+class ParentComponent extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={markimage} className="markimage" alt="markimage" />
+          <a
+            className="App-link"
+            href="https://www.reverbnation.com/TheNuKendalls"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           Mark Kendall Band
+          </a>
+          
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p> */}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React for the Best Job on the planet! Mr Mark Kendall of KCF Technologies.
+          </a>
+          <a
+            className="App-link"
+            href="https://kendallsoft-dd2b9.firebaseapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My Angular Site
+          </a>
+          
+        </header>
+      </div>
+    );
+  }
+  }
 
-export default App;
+export default ParentComponent;
+
