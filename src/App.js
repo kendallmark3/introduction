7dfadb55b8fdb7ef9cde736053ae986fad225078
Mark Kendall <mark.kendall@kcftech.com>
2020-05-16 16:24:59 -0500

    csschanges

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={markimage} width="300px" height="400px" alt="markimage" />
+        <img src={markimage} className="markimage" alt="markimage" />
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
