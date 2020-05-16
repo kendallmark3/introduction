539051f0a6a1bd2163a4d5e58cc7cec13bfafa9f
Mark Kendall <mark.kendall@kcftech.com>
2020-05-16 16:19:28 -0500

    Addimage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import markimage from './MarkKendall.jpg';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={markimage} width="300px" height="400px" alt="markimage" />
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
